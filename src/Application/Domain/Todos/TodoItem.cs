3958f6a2102fb8d01c2953940911d268859025c2
Stanislav Zeman <zeman@standa.dev>
2024-02-10 15:05:18 +0100

    revert: 1a36c4b33f0794275361cf5d2c1547c23e54dda8
    This reverts commit 1a36c4b33f0794275361cf5d2c1547c23e54dda8.

---
+++ src/Application/Domain/Todos/TodoItem.cs
@@ -0,0 +1,63 @@
+﻿using Cumio.Application.Common;
+using Cumio.Application.Common.Mappings;
+
+namespace Cumio.Application.Domain.Todos;
+
+public class TodoItem : AuditableEntity, IHasDomainEvent
+{
+    public int Id { get; set; }
+
+    public int ListId { get; set; }
+
+    public string? Title { get; set; }
+
+    public string? Note { get; set; }
+
+    public PriorityLevel Priority { get; set; }
+
+    public DateTime? Reminder { get; set; }
+
+    private bool _done;
+    public bool Done
+    {
+        get => _done;
+        set
+        {
+            if (value && _done == false)
+            {
+                DomainEvents.Add(new TodoItemCompletedEvent(this));
+            }
+
+            _done = value;
+        }
+    }
+
+    public TodoList List { get; set; } = null!;
+
+    public List<DomainEvent> DomainEvents { get; } = new List<DomainEvent>();
+}
+
+public class TodoItemCompletedEvent : DomainEvent
+{
+    public TodoItemCompletedEvent(TodoItem item)
+    {
+        Item = item;
+    }
+
+    public TodoItem Item { get; }
+}
+
+public enum PriorityLevel
+{
+    None = 0,
+    Low = 1,
+    Medium = 2,
+    High = 3
+}
+
+public class TodoItemRecord : IMapFrom<TodoItem>
+{
+    public string? Title { get; set; }
+
+    public bool Done { get; set; }
+}
