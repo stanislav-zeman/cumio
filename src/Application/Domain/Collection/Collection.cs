015ae69b829f234b55e82668bf5902a40415eb31
Marek Dlouhý <marek.dlouhy@plus4u.net>
2024-02-10 13:34:11 +0100

    feat: Content and Collection application entities

---
+++ src/Application/Domain/Collection/Collection.cs
@@ -0,0 +1,51 @@
+﻿using Cumio.Application.Common;
+using Cumio.Application.Common.Mappings;
+
+namespace Cumio.Application.Domain.Todos;
+
+public class Collection : AuditableEntity, IHasDomainEvent
+{
+    public int Id { get; set; }
+
+    public int AuthorId { get; set; }
+
+    public string? Title { get; set; }
+
+    public List<int> Contents { get; set; } = new List<int>();
+    public List<DomainEvent> DomainEvents { get; } = new List<DomainEvent>();
+}
+
+public class CollectionCreatedEvent : DomainEvent
+{
+    public CollectionCreatedEvent(Collection item)
+    {
+
+    }
+
+    public Collection Item { get; }
+}
+
+public class CollectionEditedEvent : DomainEvent
+{
+    public CollectionEditedEvent(Collection item)
+    {
+
+    }
+
+    public Collection Item { get; }
+}
+
+public class CollectionDeletedEvent : DomainEvent
+{
+    public CollectionDeletedEvent(Collection item)
+    {
+
+    }
+
+    public Collection Item { get; }
+}
+public class TodoItemRecord : IMapFrom<Collection>
+{
+    public string? Title { get; set; }
+
+}
