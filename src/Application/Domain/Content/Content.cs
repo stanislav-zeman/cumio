1a36c4b33f0794275361cf5d2c1547c23e54dda8
Marek Dlouhý <marek.dlouhy@plus4u.net>
2024-02-10 13:34:11 +0100

    feat: Content and Collection application entities

---
+++ src/Application/Domain/Content/Content.cs
@@ -0,0 +1,74 @@
+﻿using Cumio.Application.Common;
+using Cumio.Application.Common.Mappings;
+
+namespace Cumio.Application.Domain.Todos;
+
+public class Content : AuditableEntity, IHasDomainEvent
+{
+    public int Id { get; set; }
+    public string? Title { get; set; }
+    public int AuthorId { get; set; }
+    public string? Url { get; set; }
+    public int Length {  get; set; }
+    public ContentType ContentType { get; set; }
+    public List<DomainEvent> DomainEvents { get; } = new List<DomainEvent>();
+}
+
+public class ContentSearchedEvent: DomainEvent
+{
+    public ContentSearchedEvent(Content item)
+    {
+       
+    }
+
+    public Content Item { get; }
+}
+
+public class ContentCreatedEvent : DomainEvent
+{
+    public ContentCreatedEvent(Content item)
+    {
+
+    }
+
+    public Content Item { get; }
+}
+
+public class ContentEditedEvent : DomainEvent
+{
+    public ContentEditedEvent(Content item)
+    {
+
+    }
+
+    public Content Item { get; }
+}
+
+public class ContentViewedEvent : DomainEvent
+{
+    public ContentViewedEvent(Content item)
+    {
+
+    }
+
+    public Content Item { get; }
+}
+
+public class ContentSharedEvent : DomainEvent
+{
+    public ContentSharedEvent(Content item)
+    {
+
+    }
+
+    public Content Item { get; }
+}
+
+public class ContentRecord : IMapFrom<Content>
+{
+    public string? Title { get; set; }
+}
+
+public enum ContentType { 
+    Song, Podcast, Audiobook
+}
