abcb639dd1d9284706d51e312521d36a9888819d
Stanislav Zeman <zeman@standa.dev>
2024-02-11 21:36:04 +0100

    test: add working docs scenario for K6

--- tests/k6-api.js
+++ tests/k6-api.js
@@ -2,20 +2,28 @@ import { check } from 'k6';
 import { Httpx } from 'https://jslib.k6.io/httpx/0.1.0/index.js';
 
 export const options = {
-    iterations: 1,
+   thresholds: {
+        checks: ['rate>0.95'],
+    },
+    scenarios: {
+        docs: {
+          executor: 'constant-vus',
+          exec: 'docs',
+          vus: 1,
+          duration: '5s',
+          gracefulStop: '5s',
+        },
+    },
 };
 
 const session = new Httpx({
-    baseURL:  __ENV.BASE_URL || "http://api:8080"
+    baseURL:  __ENV.BASE_URL || "http://api:8080", // docker compose service name
+    timeout: 1000, // 1s
 })
 
-export default function testSuite() {
-    pingSwaggerDocs()
-}
-
-function pingSwaggerDocs() {
-    const response = session.get("/index.html");
-    check(response, {
+export function docs() {
+    const res = session.get("/index.html");
+    check(res, {
         'is status 200': (r) => r.status === 200,
     });
 }
