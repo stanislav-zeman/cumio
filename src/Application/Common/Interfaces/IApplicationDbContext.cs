d0030687fa134f4c111b4a6321a1476c0b11496f
Stanislav Zeman <zeman@standa.dev>
2024-02-15 02:48:31 +0100

    feat(auth): integrate identity service

---
+++ src/Application/Common/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,13 @@
+using Cumio.Application.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cumio.Application.Common.Interfaces;
+
+public interface IApplicationDbContext
+{
+    DbSet<TodoList> TodoLists { get; }
+
+    DbSet<TodoItem> TodoItems { get; }
+
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+}
