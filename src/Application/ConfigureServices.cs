fe0d28dd28984d9cc0ef9f546ff14f0998c485d3
Stanislav Zeman <zeman@standa.dev>
2024-02-15 03:36:55 +0100

    chore(services): remove datetime service

--- src/Application/ConfigureServices.cs
+++ src/Application/ConfigureServices.cs
@@ -46,7 +46,6 @@ public static class DependencyInjection
         services.AddScoped<IDomainEventService, DomainEventService>();
         services.AddScoped<IDomainEventService, DomainEventService>();
 
-        services.AddTransient<IDateTime, DateTimeService>();
 
         services.AddAuthentication()
             .AddBearerToken(IdentityConstants.BearerScheme);
