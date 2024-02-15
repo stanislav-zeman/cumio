4080fb44ee951fbca7d482ca94b1bad537d20f35
Stanislav Zeman <zeman@standa.dev>
2024-02-15 03:04:33 +0100

    feat(behavior): wire identity service to behavior

--- src/Application/Common/Behaviours/LoggingBehaviour.cs
+++ src/Application/Common/Behaviours/LoggingBehaviour.cs
@@ -7,20 +7,28 @@ namespace Cumio.Application.Common.Behaviours;
 public class LoggingBehaviour<TRequest> : IRequestPreProcessor<TRequest> where TRequest : notnull
 {
     private readonly ILogger _logger;
-    private readonly ICurrentUserService _currentUserService;
+    private readonly IUser _user;
+    private readonly IIdentityService _identityService;
 
-    public LoggingBehaviour(ILogger<TRequest> logger, ICurrentUserService currentUserService)
+    public LoggingBehaviour(ILogger<TRequest> logger, IUser user, IIdentityService identityService)
     {
         _logger = logger;
-        _currentUserService = currentUserService;
+        _user = user;
+        _identityService = identityService;
     }
 
-    public Task Process(TRequest request, CancellationToken cancellationToken)
+    public async Task Process(TRequest request, CancellationToken cancellationToken)
     {
         var requestName = typeof(TRequest).Name;
-        var userId = _currentUserService.UserId ?? string.Empty;
+        var userId = _user.Id ?? string.Empty;
+        string? userName = string.Empty;
 
-        return Task.Run(() => _logger.LogInformation("VerticalSlice Request: {Name} {@UserId} {@Request}",
-                requestName, userId, request), cancellationToken);
+        if (!string.IsNullOrEmpty(userId))
+        {
+            userName = await _identityService.GetUserNameAsync(userId);
+        }
+
+        _logger.LogInformation("CleanArchitecture Request: {Name} {@UserId} {@UserName} {@Request}",
+            requestName, userId, userName, request);
     }
 }
