4ddb319e4979e0e54c77ac4e204ec3cc61b5005a
Stanislav Zeman <zeman@standa.dev>
2024-02-15 15:49:08 +0100

    feat(events): create event handler for content creation

---
+++ src/Application/Features/Contents/EventHandlers/TodoItemCreatedEventHandler.cs
@@ -0,0 +1,24 @@
+﻿using Cumio.Application.Common.Models;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace Cumio.Application.Features.Contents.EventHandlers;
+
+public class ContentCreatedEventHandler : INotificationHandler<DomainEventNotification<ContentCreatedEvent>>
+{
+    private readonly ILogger<ContentCreatedEventHandler> _logger;
+
+    public ContentCreatedEventHandler(ILogger<ContentCreatedEventHandler> logger)
+    {
+        _logger = logger;
+    }
+
+    public Task Handle(DomainEventNotification<ContentCreatedEvent> notification, CancellationToken cancellationToken)
+    {
+        var domainEvent = notification.DomainEvent;
+
+        _logger.LogInformation("VerticalSlice Domain Event: {DomainEvent}", domainEvent.GetType().Name);
+
+        return Task.CompletedTask;
+    }
+}
